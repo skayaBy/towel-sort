154e0752413f215977872f6b47c74016bc6aa315
Zavadskaya Olga <zavadskaya.ov@gmail.com>
2023-01-27 14:30:24 +0300

    refactor: change solution

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ module.exports = function towelSort (matrix) {
 
   let arr=[];
   for (let i=0; i<matrix.length; i++) {
-    if (i%2!=0) {
+     /* if (i%2!=0) {
       matrix[i].sort(function(a, b){return b-a});
-    }
+    }*/
     for (let j=0; j<matrix[i].length; j++) {
-         arr.push(matrix[i][j]);
+        let ind = i%2 === 0 ? j  : (matrix[i].length - j - 1)
+        // arr.push(matrix[i][j]);
+         arr.push(matrix[i][ind]);
     }
    }
   return arr;
